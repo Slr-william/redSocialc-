a9878981c86b0a503897290984db3a3c62084080
Slr-william <w.as.g@utp.edu.co>
2016-12-09 18:05:37 -0500

    agregadas nuevas structuras

--- redSocial.cpp
+++ redSocial.cpp
@@ -1,12 +1,36 @@
 #include<iostream>
 
+#define TRED 100
+
 using namespace std;
 
-struct{
-  char[30] correo;
-  char[30] contraseña;
-  
+struct perfil{
+  char* correo = "algo";
+  char* pass = "algo";
+  char* nombre = "algo";
+  char* sexo = "algo";
+  char* edad = "algo";
+  char* ciudad = "algo";
+  perfil* amigos[30];
 };
+
+struct redSocial{
+  perfil red[TRED];
+};
+
+  void crearPerfil(char* correo, char* pass, char* nombre, char* sexo, char* edad, char* ciudad) {
+    perfil p;
+    p.correo = correo;
+    p.pass = pass;
+    p.nombre = nombre;
+    p.sexo = sexo;
+    p.edad = edad;
+    p.ciudad = ciudad;
+  }
+
+void agregarAmigos(perfil &p) {
+
+}
 
 int main(int argc, char const *argv[]) {
   return 0;
